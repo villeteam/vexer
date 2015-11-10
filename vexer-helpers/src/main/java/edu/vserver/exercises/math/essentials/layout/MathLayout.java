b122adb30b23865ad56efdbcafad8f87ad183913
Erno Lokkila <eolokk@utu.fi>
2015-11-10 14:24:35 +0200

    Enter now moves to the next question and checks the answer in mathlayout. Code format

--- vexer-helpers/src/main/java/edu/vserver/exercises/math/essentials/layout/MathLayout.java
+++ vexer-helpers/src/main/java/edu/vserver/exercises/math/essentials/layout/MathLayout.java
@@ -7,7 +7,6 @@ import org.vaadin.jouni.animator.AnimatorProxy.AnimationListener;
 import org.vaadin.jouni.animator.shared.AnimType;
 
 import com.vaadin.event.ShortcutAction.KeyCode;
-import com.vaadin.event.ShortcutAction.ModifierKey;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
@@ -28,11 +27,11 @@ import fi.utu.ville.standardutils.UIConstants;
 
 public class MathLayout<P extends Problem> extends VerticalLayout
 		implements MathLayoutController {
-
+		
 	private static final long serialVersionUID = 2062189376088801532L;
-
+	
 	private HorizontalLayout hLayout;
-
+	
 	private ExecutionSettings execSettings;
 	private AbstractMathExecutor<?, ?, P> exerExecutor;
 	private final HorizontalLayout buttonBar;
@@ -43,62 +42,57 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 	public Animation aPrev, aNext, aCheck;
 	public static final int shiftSize = 2000;
 	private final TimeStampHandler timeStampHandler;
-
+	
 	public MathLayout(final Localizer localizer,
 			final MathExerciseView<P> exView,
 			final MathExerciseState<P> exState,
 			TimeStampHandler timeStampHandler,
 			final ExecutionSettings execSettings) {
-
+			
 		this.execSettings = execSettings;
 		exView.setLayoutController(this);
-
+		
 		checkBtn = StandardUIFactory.getButton(
 				localizer.getUIText(UIConstants.MATH_CHECK), Icon.MATH_CHECK);
-
-		// Enter listener for checking answer
-		checkBtn.setClickShortcut(KeyCode.ENTER);
-
+				
 		nextBtn = StandardUIFactory.getButton(
 				localizer.getUIText(UIConstants.MATH_NEXT), Icon.MATH_NEXT);
-		// Right key listener for moving forward
-		// Not working for some reason :(
-		nextBtn.setClickShortcut(KeyCode.ARROW_RIGHT, ModifierKey.ALT);
-
+				
 		prevBtn = StandardUIFactory.getButton(
 				localizer.getUIText(UIConstants.MATH_PREV), Icon.MATH_PREV);
-
+				
+		setFocusAndShortcut(checkBtn);
+		
 		// innerLayout = new VerticalLayout();
 		innerLayout = new VerticalLayout();
 		innerLayout.setStyleName("math-layout-margin");
-
+		
 		this.timeStampHandler = timeStampHandler;
-
+		
 		proxy = new AnimatorProxy();
-
+		
 		proxy.addListener(new AnimationListener() {
 			@Override
 			public void onAnimation(AnimationEvent event) {
-
 				if (event.getAnimation() == aNext) {
-
+					
 					exView.clearFields();
-
+					
 					prevBtn.setEnabled(false); // Change if browsing is
 												// possible.
 					nextBtn.setEnabled(false);
-
+					
 					progress.nextStep(exState.getCurrentProblem().isCorrect());
-
+					
 					HorizontalLayout old = hLayout;
 					hLayout = new HorizontalLayout();
 					// hLayout.setStyleName("base");
-
+					
 					// Math navigation bar
 					// - previous button
 					// - check button
 					// - next button
-
+					
 					hLayout.addComponent(innerLayout);
 					hLayout.setComponentAlignment(innerLayout,
 							Alignment.MIDDLE_CENTER);
@@ -106,17 +100,18 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 					replaceComponent(old, hLayout);
 					setComponentAlignment(hLayout, Alignment.MIDDLE_CENTER);
 					setComponentAlignment(buttonBar, Alignment.MIDDLE_LEFT);
-
+					
 					proxy.animate(hLayout, AnimType.FADE_IN).setDuration(500)
 							.setDelay(20);
 					checkBtn.setEnabled(true);
+					setFocusAndShortcut(checkBtn);
 					exView.drawProblem(exState.nextProblem());
-
+					
 					exerExecutor
 							.startMathPerformance(exState.getCurrentProblem());
-
+							
 				} else if (event.getAnimation() == aPrev) {
-
+					
 					exView.clearFields();
 					prevBtn.setEnabled(false); // Change if browsing is
 												// possible.
@@ -125,54 +120,53 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 					HorizontalLayout old = hLayout;
 					hLayout = new HorizontalLayout();
 					hLayout.setStyleName("base");
-
+					
 					// buttonBar.addComponent(prevBtn);
 					hLayout.addComponent(innerLayout);
 					hLayout.setComponentAlignment(innerLayout,
 							Alignment.MIDDLE_CENTER);
 					hLayout.setStyleName("makeItTransparent");
-
+					
 					replaceComponent(old, hLayout);
 					setComponentAlignment(hLayout, Alignment.MIDDLE_CENTER);
-
+					
 					proxy.animate(hLayout, AnimType.FADE_IN).setDuration(500)
 							.setDelay(20);
-
+							
 				} else if (event.getAnimation() == aCheck) {
 					exState.tryAnswer(exView.getAnswer());
 					exerExecutor.setUnsubmittedChangesFlag(true);
-
+					
 					if (!execSettings.isNoCorrAnswers()) {
 						exView.showSolution(exState.getCurrentProblem());
 					}
 					exerExecutor
 							.finishMathPerformance(exState.getCurrentProblem());
-
+							
 					MathLayout.this.timeStampHandler
 							.add("Question" + TimeStampHandler.separator
 									+ exState.getCurrentProblem().getQuestion(
 											localizer)
-							+ TimeStampHandler.separator + "UserAnswer"
-							+ TimeStampHandler.separator
-							+ exState.getCurrentProblem().getUserAnswer()
-							+ TimeStampHandler.separator + "CorrectAnswer"
-							+ TimeStampHandler.separator
-							+ exState.getCurrentProblem().getCorrectAnswer()
-							+ TimeStampHandler.separator + "isCorrect"
-							+ TimeStampHandler.separator
-							+ exState.getCurrentProblem().isCorrect());
-
+									+ TimeStampHandler.separator + "UserAnswer"
+									+ TimeStampHandler.separator
+									+ exState.getCurrentProblem().getUserAnswer()
+									+ TimeStampHandler.separator + "CorrectAnswer"
+									+ TimeStampHandler.separator
+									+ exState.getCurrentProblem().getCorrectAnswer()
+									+ TimeStampHandler.separator + "isCorrect"
+									+ TimeStampHandler.separator
+									+ exState.getCurrentProblem().isCorrect());
+									
 					checkBtn.setEnabled(false);
-
+					
 					if (exState.hasNextProblem()) {
 						nextBtn.setEnabled(true);
+						setFocusAndShortcut(nextBtn);
 					} else {
 						exView.lockControls();
-					}
-					if (!exState.hasNextProblem()) {
 						progress.nextStep(
 								exState.getCurrentProblem().isCorrect());
-
+								
 						final Window confirm = new Window(localizer
 								.getUIText(UIConstants.SUBMIT_QUESTION));
 						confirm.setModal(true);
@@ -183,7 +177,7 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 						confirm.addStyleName("unclosable-window");
 						confirm.setClosable(false);
 						confirm.addStyleName("submit-window");
-
+						
 						VerticalLayout content = new VerticalLayout();
 						content.setSizeFull();
 						content.setMargin(true);
@@ -199,17 +193,17 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 						// .getInformationPanel(localizer.getUIText(
 						// UIConstants.SUBMIT_QUESTION_DESCR)));
 						confirm.setContent(content);
-
+						
 						// Button holder (back to round, replay, next)
 						HorizontalLayout buttons = new HorizontalLayout();
 						buttons.addStyleName("submit-buttons-container");
 						buttons.setWidth("100%");
 						buttons.setSpacing(true);
-
+						
 						content.addComponent(buttons);
 						content.setComponentAlignment(buttons,
 								Alignment.TOP_CENTER);
-
+								
 						Button submit = StandardUIFactory
 								.getRoundButton(Icon.SUBMIT);
 						submit.setDescription(
@@ -218,23 +212,23 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 								.getRoundButton(Icon.CLOSE);
 						cancel.setDescription(
 								localizer.getUIText(UIConstants.CLOSE));
-
+								
 						buttons.addComponents(submit, cancel);
 						buttons.setComponentAlignment(submit,
 								Alignment.MIDDLE_CENTER);
 						buttons.setComponentAlignment(cancel,
 								Alignment.MIDDLE_CENTER);
-
+								
 						submit.addClickListener(new Button.ClickListener() {
-
+							
 							private static final long serialVersionUID = -1372389156569716860L;
-
+							
 							@Override
 							public void buttonClick(ClickEvent event) {
-
+								
 								exerExecutor.askSubmit(
 										StandardSubmissionType.NORMAL);
-
+										
 								// listenerHelper.informOnlySubmit(checkCorrectness(),
 								// parseSubmissionInfo(),
 								// StandardSubmissionType.NORMAL,
@@ -242,66 +236,84 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 								confirm.close();
 							}
 						});
-
+						
 						cancel.addClickListener(new Button.ClickListener() {
-
+							
 							/**
 							 * 
 							 */
 							private static final long serialVersionUID = 6537215144432359150L;
-
+							
 							@Override
 							public void buttonClick(ClickEvent event) {
 								confirm.close();
 							}
 						});
-
+						
 						UI.getCurrent().addWindow(confirm);
 					}
-
+					
 					proxy.animate(exView, AnimType.FADE_IN).setDuration(250)
 							.setDelay(50);
-
+							
 				}
 			}
 		});
-
+		
 		// Constructors.
 		progress = new MathAssignmentProgressBar(exState.getProblemCount(),
 				!execSettings.isNoCorrAnswers());
-
+				
 		hLayout = new HorizontalLayout();
 		buttonBar = StandardUIFactory.getButtonPanelForMathControls(checkBtn,
 				nextBtn, progress);
-
+				
 		prevBtn.setEnabled(false);
 		nextBtn.setEnabled(false);
-
+		
 		// Component hierarchy.
 		innerLayout.addComponent(exView);
 		addComponent(proxy);
 		// hLayout.setStyleName("base");
-
+		
 		// hLayout.addComponent(prevBtn);
 		addComponent(buttonBar);
 		hLayout.addComponent(innerLayout);
 		// hLayout.addComponent(nextBtn);
 		addComponent(hLayout);
-
+		
 		// Alignment properties.
 		innerLayout.setComponentAlignment(exView, Alignment.MIDDLE_CENTER);
 		hLayout.setComponentAlignment(innerLayout, Alignment.MIDDLE_CENTER);
 		setComponentAlignment(hLayout, Alignment.MIDDLE_CENTER);
 	}
-
+	
+	/**
+	 * Removes all ClickShortcuts for the check and next buttons. Adds Enter as the ClickShortcut for the given button.
+	 * 
+	 * @param b
+	 *            the button to add Enter as a ClickShortcut. f null, all ClickShortcuts are removed
+	 */
+	private void setFocusAndShortcut(Button b) {
+		checkBtn.removeClickShortcut();
+		nextBtn.removeClickShortcut();
+		System.out.println("removed click shortcuts");
+		if (b != null) {
+			System.out.println("set button " + b.getCaption());
+			b.setClickShortcut(KeyCode.ENTER);
+			b.focus();
+			//			b.focus();
+		}
+	}
+	
 	public MathAssignmentProgressBar getProgressBar() {
 		return progress;
 	}
-
+	
 	public void attachExecutor(AbstractMathExecutor<?, ?, P> exerExecutor) {
 		this.exerExecutor = exerExecutor;
 	}
-
+	
 	public void attachControllers(PrevButtonListener prevController,
 			CheckButtonListener checkController,
 			NextButtonListener nextController) {
@@ -310,43 +322,43 @@ public class MathLayout<P extends Problem> extends VerticalLayout
 		nextBtn.addClickListener(nextController);
 		// opt2.addListener(controller);
 	}
-
+	
 	public void attachExecSettings(ExecutionSettings execSettings) {
 		this.execSettings = execSettings;
 	}
-
+	
 	public Layout getInnerLayout() {
 		return innerLayout;
 	}
-
+	
 	public Layout getHLayout() {
 		return hLayout;
 	}
-
+	
 	public AnimatorProxy getProxy() {
 		return proxy;
 	}
-
+	
 	public Button getPrevBtn() {
 		return prevBtn;
 	}
-
+	
 	public Button getNextBtn() {
 		return nextBtn;
 	}
-
+	
 	@Override
 	public Button getCheckBtn() {
 		return checkBtn;
 	}
-
+	
 	public void reset() {
 		prevBtn.setEnabled(false);
 		checkBtn.setEnabled(true);
 		nextBtn.setEnabled(false);
 		progress.reset();
 	}
-
+	
 	@Override
 	public void setCheckButtonEnabled(boolean enabled) {
 		checkBtn.setEnabled(enabled);
